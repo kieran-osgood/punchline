fbec6be78d79a1889baf88fe115e4812c1c0d8cf
Kieran Osgood <kieran@analogrepublic.com>
2020-08-01 15:12:01 +0100

    chore: move mocks folder

--- src/app/main/screens/__tests__/settings.test.tsx
+++ src/app/main/screens/__tests__/settings.test.tsx
@@ -2,8 +2,8 @@ import { View } from 'react-native';
 import React from 'react';
 import { render } from '@testing-library/react-native';
 
-import '../../../../__mocks__/react-native-firebase'; // MUST come before <App /> import
-import '../../../../__mocks__/react-native-comunity'; // MUST come before <App /> import
+import '../../../../../__mocks__/react-native-firebase'; // MUST come before <App /> import
+import '../../../../../__mocks__/react-native-comunity'; // MUST come before <App /> import
 import Settings from '../settings';
 
 jest.useFakeTimers();
