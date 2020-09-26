16533bddb56ee56ee1c0ec3203e5230cae9728a2
Kieran Osgood <kieran@analogrepublic.com>
2020-09-26 12:49:18 +0100

    feat(SafeAreaProvider): add SafeAreaProvider to wrap navigation

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,10 +7,10 @@ import codePush from 'react-native-code-push';
 
 import { AuthStack } from './app/auth/auth-stack';
 import MainTabs from './app/main/main-tabs';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 GoogleSignin.configure({
   webClientId:
-    // '681986405885-eb59nigfp7jejgk8cn02dqor9jm2sqft.apps.googleusercontent.com',
     '681986405885-dhai19n3c3kai1ad2i5l6u57ot14uorq.apps.googleusercontent.com',
   iosClientId:
     '681986405885-oms8c4edds7s1cjlm550ss1sa8rqui7d.apps.googleusercontent.com',
@@ -29,9 +29,11 @@ const Routes = () => {
 
   return (
     <ThemeProvider>
-      <NavigationContainer>
-        {!user ? <AuthStack /> : <MainTabs />}
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          {!user ? <AuthStack /> : <MainTabs />}
+        </NavigationContainer>
+      </SafeAreaProvider>
     </ThemeProvider>
   );
 };
