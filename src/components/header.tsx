78c2b7f7ca2d88572c48ac76d207de58d509e924
Kieran Osgood <kieran@analogrepublic.com>
2020-11-16 17:07:15 +0000

    feat(AppLogo): size based on device density pixel

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,15 @@ import AppLogo from 'components/app-logo';
 import React from 'react';
 import { View, ViewStyle } from 'react-native';
 import { spacing } from 'theme';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 const Header = () => (
   <>
     <View style={LOGO_CONTAINER}>
-      <AppLogo style={LOGO} width={130} />
+      <AppLogo style={LOGO} height={hp('5%')} width={wp('35%')} />
     </View>
     <BannerAd
       unitId={TestIds.BANNER} // ! Setup to check process.env
@@ -18,14 +22,12 @@ const Header = () => (
 
 const LOGO_CONTAINER: ViewStyle = {
   width: '100%',
-  // flex: 0.1,
 };
 
 const LOGO: ViewStyle = {
   padding: spacing[1],
   alignSelf: 'auto',
   justifyContent: 'flex-start',
-  // flex: 0.2,
 };
 
 export default Header;
