4d747902de5eb29b2f0d129586cc904836d1f147
Kieran Osgood <kieran@analogrepublic.com>
2020-12-23 16:18:26 +0000

    refactor(Settings): componentise settings section
    (cherry picked from commit 62da7bcde14bfe47c678ab1ee842fd509045b676)

--- src/app/main/screens/settings.tsx
+++ src/app/main/screens/settings.tsx
@@ -23,6 +23,69 @@ import { LocalStorageKeys } from 'src/types';
 type SoundSetting = 'muted' | 'unmuted';
 
 export default function Settings() {
+  return (
+    <CenterView>
+      <ScrollView style={{ paddingTop: wp('10%') }}>
+        <Text h3 style={{ alignSelf: 'center' }}>
+          Settings
+        </Text>
+        <CenterView
+          style={{
+            width: wp('90%'),
+            paddingVertical: hp('2.5%'),
+          }}>
+          <CategorySetting />
+          <SoundSetting />
+          <LogoutButton />
+        </CenterView>
+      </ScrollView>
+    </CenterView>
+  );
+}
+
+const SoundSetting = () => {
+  const [soundSetting, setSoundSetting] = React.useState<SoundSetting>();
+  const { getItem, setItem } = useAsyncStorage(LocalStorageKeys.soundIsMuted);
+
+  const storeData = async (value: SoundSetting) => {
+    console.log('value: ', value);
+    await setItem(String(value));
+  };
+  React.useEffect(() => {
+    getItem().then((res) => {
+      setSoundSetting(res as SoundSetting);
+    });
+  }, [getItem]);
+
+  return (
+    <>
+      <CenterView
+        style={{
+          width: '100%',
+          paddingRight: spacing[3],
+          marginBottom: hp('2.5%'),
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          borderBottomColor: '#fff',
+          borderBottomWidth: 10,
+        }}>
+        <Text h4>Sound</Text>
+        <Icon
+          name={soundSetting === 'muted' ? 'volume-mute' : 'volume-up'}
+          size={35}
+          onPress={() => {
+            setSoundSetting((current) =>
+              current === 'muted' ? 'unmuted' : 'muted',
+            );
+            storeData(soundSetting === 'muted' ? 'unmuted' : 'muted');
+          }}
+        />
+      </CenterView>
+    </>
+  );
+};
+
+const CategorySetting = () => {
   const { userCategories, categories: apiCategories } = useCategoriesContext();
   const [categories, setCategories] = React.useState<
     CategorySettings[] | undefined
@@ -35,14 +98,6 @@ export default function Settings() {
       return { ...category, isActive: false };
     }),
   );
-  const [soundSetting, setSoundSetting] = React.useState<SoundSetting>();
-  const { getItem, setItem } = useAsyncStorage(LocalStorageKeys.soundIsMuted);
-
-  React.useEffect(() => {
-    getItem().then((res) => {
-      setSoundSetting(res as SoundSetting);
-    });
-  }, [getItem]);
 
   const handleValueChanged = async (value: CategorySettings[]) => {
     const user = await getCurrentUser(false);
@@ -50,68 +105,34 @@ export default function Settings() {
     setCategories(value);
   };
 
-  const storeData = async (value: SoundSetting) => {
-    console.log('value: ', value);
-    await setItem(String(value));
-  };
-
   return (
-    <CenterView>
-      <ScrollView
-        style={{
-          paddingTop: wp('10%'),
-        }}>
-        <Text h3 style={{ alignSelf: 'center' }}>
-          Settings
+    <>
+      <CenterView style={{ marginBottom: hp('2.5%') }}>
+        <Text h4 style={{ alignSelf: 'flex-start' }}>
+          Categories
         </Text>
-        <CenterView
-          style={{
-            width: wp('90%'),
-            paddingVertical: hp('2.5%'),
-          }}>
-          <CenterView
-            style={{
-              width: '100%',
-              paddingRight: spacing[3],
-              marginBottom: hp('2.5%'),
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              borderBottomColor: '#fff',
-              borderBottomWidth: 10,
-            }}>
-            <Text h4>Sound</Text>
-            <Icon
-              name={soundSetting === 'muted' ? 'volume-mute' : 'volume-up'}
-              size={35}
-              onPress={() => {
-                setSoundSetting((current) =>
-                  current === 'muted' ? 'unmuted' : 'muted',
-                );
-                storeData(soundSetting === 'muted' ? 'unmuted' : 'muted');
-              }}
-            />
-          </CenterView>
-          <CenterView style={{ marginBottom: hp('2.5%') }}>
-            <Text h4 style={{ alignSelf: 'flex-start' }}>
-              Categories
-            </Text>
-            <SelectPills
-              data={categories ?? []}
-              onValueChange={(value) => handleValueChanged(value)}
-            />
-          </CenterView>
-          <View>
-            <Button
-              buttonStyle={PILL_BUTTON}
-              titleStyle={{ color: color.text }}
-              containerStyle={BUTTON_CONTAINER}
-              onPress={() => auth().signOut()}
-              title="Logout"
-              raised
-            />
-          </View>
-        </CenterView>
-      </ScrollView>
-    </CenterView>
+        <SelectPills
+          data={categories ?? []}
+          onValueChange={(value) => handleValueChanged(value)}
+        />
+      </CenterView>
+    </>
   );
-}
+};
+
+const LogoutButton = () => {
+  return (
+    <>
+      <View>
+        <Button
+          buttonStyle={PILL_BUTTON}
+          titleStyle={{ color: color.text }}
+          containerStyle={BUTTON_CONTAINER}
+          onPress={() => auth().signOut()}
+          title="Logout"
+          raised
+        />
+      </View>
+    </>
+  );
+};
