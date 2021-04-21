19d9846486fa7d9a5c7a359937ea86562ec2aae8
Kieran Osgood <kieranbosgood@gmail.com>
2021-04-21 19:43:18 +0100

    feat: upgrade to react-native 0.64.0

--- app/screens/joke/joke-screen.tsx
+++ app/screens/joke/joke-screen.tsx
@@ -2,25 +2,18 @@ import React from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
 import { Screen, CenterView, Header, JokeSection } from "components"
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "../../models"
-import { color } from "../../theme"
+import { color } from "theme"
 
 const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
+  backgroundColor: color.background,
   flex: 1,
 }
 
 export const JokeScreen = observer(function JokeScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
-
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
   return (
     <Screen style={ROOT} preset="scroll">
+      <Header />
       <CenterView>
-        <Header />
         <JokeSection />
       </CenterView>
     </Screen>
