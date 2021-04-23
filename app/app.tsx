2a0c5a94634b4b8688103d81e0925e00f95a26ab
Kieran Osgood <kieranbosgood@gmail.com>
2021-04-23 15:05:34 +0100

    feat: configure Service Provider to tidy initiallisation logic

--- app/app.tsx
+++ app/app.tsx
@@ -1,11 +1,4 @@
 /**
- * Welcome to the main entry point of the app. In this file, we'll
- * be kicking off our app.
- *
- * Most of this file is boilerplate and you shouldn't need to modify
- * it very often. But take some time to look through and understand
- * what is going on here.
- *
  * The app navigation resides in ./app/navigators, so head over there
  * if you're interested in adding screens and navigators.
  */
@@ -14,7 +7,7 @@ import "./utils/ignore-warnings"
 import React, { useState, useEffect, useRef } from "react"
 import { NavigationContainerRef } from "@react-navigation/native"
 import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context"
-import { initFonts } from "./theme/fonts" // expo
+// import { initFonts } from "./theme/fonts" // expo
 import * as storage from "./utils/storage"
 import {
   useBackButtonHandler,
@@ -25,55 +18,14 @@ import {
 } from "./navigators"
 import { RootStore, RootStoreProvider, setupRootStore, useStores } from "./models"
 import { ToggleStorybook } from "../storybook/toggle-storybook"
-
-import admob, { MaxAdContentRating } from "@react-native-firebase/admob"
-import { GoogleSignin } from "@react-native-community/google-signin"
 import auth from "@react-native-firebase/auth"
-import { RootStore as RootGraphqlStore } from "./graphql/RootStore"
 import { StoreContext, useQuery } from "./graphql/reactUtils"
-
-// This puts screens in a native ViewController or Activity. If you want fully native
-// stack navigation, use `createNativeStackNavigator` in place of `createStackNavigator`:
-// https://github.com/kmagiera/react-native-screens#using-native-stack-navigator
-import { enableScreens } from "react-native-screens"
 import { observer } from "mobx-react-lite"
-import { createHttpClient } from "mst-gql"
-import * as Sentry from "@sentry/react-native"
-
-const packageJson = require("../package.json")
-Sentry.init({
-  dsn: "https://14d48ec94bab4f1fa583a3b6ab7f7a3b@o577022.ingest.sentry.io/5731300",
-  release: "com.ko.punchline@" + packageJson.version,
-  environment: process.env.NODE_ENV,
-  attachStacktrace: true,
-  autoSessionTracking: true,
-  enabled: !__DEV__,
-})
+import ServiceProvider from "./utils/service-provider"
 
 export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE"
 
-enableScreens()
-
-admob().setRequestConfiguration({
-  // Update all future requests suitable for parental guidance
-  maxAdContentRating: MaxAdContentRating.T,
-
-  // Indicates that you want your content treated as child-directed for purposes of COPPA.
-  tagForChildDirectedTreatment: false,
-
-  // Indicates that you want the ad request to be handled in a
-  // manner suitable for users under the age of consent.
-  tagForUnderAgeOfConsent: true,
-})
-
-GoogleSignin.configure({
-  webClientId: "681986405885-dhai19n3c3kai1ad2i5l6u57ot14uorq.apps.googleusercontent.com",
-  offlineAccess: true,
-})
-
-const rootGraphqlStore = RootGraphqlStore.create(undefined, {
-  gqlHttpClient: createHttpClient("http://localhost:5000/graphql"),
-})
+const Services = new ServiceProvider()
 
 /**
  * This is the root component of our app.
@@ -90,7 +42,7 @@ const App = observer(function App() {
   // Kick off initial async loading actions, like loading fonts and RootStore
   useEffect(() => {
     (async () => {
-      await initFonts() // expo
+      // await initFonts() // expo
       setupRootStore().then((newRootStore) => {
         newRootStore.userStore?.updateUser(auth().currentUser)
         setRootStore(newRootStore)
@@ -108,7 +60,7 @@ const App = observer(function App() {
   return (
     <ToggleStorybook>
       <RootStoreProvider value={rootStore}>
-        <StoreContext.Provider value={rootGraphqlStore}>
+        <StoreContext.Provider value={Services.rootGraphqlStore}>
           <Authorization>
             <SafeAreaProvider initialMetrics={initialWindowMetrics}>
               <RootNavigator
---
+++ app/utils/service-provider.ts
@@ -0,0 +1,52 @@
+// This puts screens in a native ViewController or Activity. If you want fully native
+// stack navigation, use `createNativeStackNavigator` in place of `createStackNavigator`:
+// https://github.com/kmagiera/react-native-screens#using-native-stack-navigator
+import { enableScreens } from "react-native-screens"
+import * as Sentry from "@sentry/react-native"
+import admob, { MaxAdContentRating } from "@react-native-firebase/admob"
+import { GoogleSignin } from "@react-native-community/google-signin"
+import { RootStore as RootGraphqlStore } from "app/graphql/RootStore"
+import { createHttpClient } from "mst-gql"
+
+const packageJson = require("package.json")
+
+export default class ServiceProvider {
+  rootGraphqlStore: ReturnType<typeof RootGraphqlStore.create>
+
+  constructor() {
+    this.initialiseVoidServices()
+    this.rootGraphqlStore = RootGraphqlStore.create(undefined, {
+      gqlHttpClient: createHttpClient("http://localhost:5000/graphql"),
+    })
+  }
+
+  initialiseVoidServices() {
+    enableScreens()
+
+    admob().setRequestConfiguration({
+      // Update all future requests suitable for parental guidance
+      maxAdContentRating: MaxAdContentRating.T,
+
+      // Indicates that you want your content treated as child-directed for purposes of COPPA.
+      tagForChildDirectedTreatment: false,
+
+      // Indicates that you want the ad request to be handled in a
+      // manner suitable for users under the age of consent.
+      tagForUnderAgeOfConsent: true,
+    })
+
+    GoogleSignin.configure({
+      webClientId: "681986405885-dhai19n3c3kai1ad2i5l6u57ot14uorq.apps.googleusercontent.com",
+      offlineAccess: true,
+    })
+
+    Sentry.init({
+      dsn: "https://14d48ec94bab4f1fa583a3b6ab7f7a3b@o577022.ingest.sentry.io/5731300",
+      release: "com.ko.punchline@" + packageJson.version,
+      environment: process.env.NODE_ENV,
+      attachStacktrace: true,
+      autoSessionTracking: true,
+      enabled: !__DEV__,
+    })
+  }
+}
