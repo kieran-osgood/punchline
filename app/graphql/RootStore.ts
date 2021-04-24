f743fbfa73848614f2d61f3d3f97917aa5ea25c5
Kieran Osgood <kieranbosgood@gmail.com>
2021-04-24 15:06:54 +0100

    feat: add nodes helper to RootStore

--- app/graphql/RootStore.ts
+++ app/graphql/RootStore.ts
@@ -3,18 +3,19 @@ import { RootStoreBase } from "./RootStore.base"
 
 export interface RootStoreType extends Instance<typeof RootStore.Type> {}
 
-export const RootStore = RootStoreBase
-  .props({
-    accessToken: types.maybeNull(types.string)
-  })
-  .actions(self => ({
+export const RootStore = RootStoreBase.props({
+  accessToken: types.maybeNull(types.string),
+})
+  .actions((self) => ({
     setAuthorizationHeader() {
-      getEnv(self).gqlHttpClient.setHeaders({ Authorization: 'bearer ' + self.accessToken })
+      getEnv(self).gqlHttpClient.setHeaders({ Authorization: "bearer " + self.accessToken })
     },
   }))
-  .actions(self => ({
-    setBearerToken(token:string) {
+  .actions((self) => ({
+    setBearerToken(token: string) {
       self.accessToken = token
       self.setAuthorizationHeader()
     },
   }))
+
+export const nodes = (val: any) => `nodes {${val}}`
