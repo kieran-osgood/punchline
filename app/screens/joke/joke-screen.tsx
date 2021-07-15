4fa962dc64b0077ee8d3e91ebc4087af61f3ccf1
Kieran Osgood <kieranbosgood@gmail.com>
2021-07-15 20:43:37 +0100

    feat(joke-screen): adjust styling on button opacities - refactor body to remove unnecessary safe zones

--- app/screens/joke/joke-screen.tsx
+++ app/screens/joke/joke-screen.tsx
@@ -3,7 +3,6 @@ import { observer } from "mobx-react-lite"
 import { Dimensions, StyleSheet, TextStyle, View, ViewStyle } from "react-native"
 import {
   Screen,
-  CenterView,
   Text,
   AdBanner,
   CircularButton,
@@ -21,15 +20,16 @@ import Animated, {
   useSharedValue,
   withSpring,
   withTiming,
+  Transition, 
+  Transitioning
 } from "react-native-reanimated"
-import Icon from "react-native-vector-icons/Feather"
 import Skip from "assets/images/skip"
 
-// import { useQuery } from "../../graphql/reactUtils"
-// import { JokeLength } from "../../graphql/JokeLengthEnum"
-// import { nodes } from "../../graphql/RootStore"
-// import { jokeModelPrimitives } from "../../graphql/JokeModel.base"
-// import { JokeModelType } from 'app/graphql'
+import { useQuery } from "../../graphql/reactUtils"
+import { JokeLength } from "../../graphql/JokeLengthEnum"
+import { nodes } from "../../graphql/RootStore"
+import { jokeModelPrimitives } from "../../graphql/JokeModel.base"
+import { JokeModelType } from "app/graphql"
 
 const { width } = Dimensions.get("screen")
 
@@ -52,21 +52,14 @@ const PAGE_GUTTERS = 15
 export const JokeScreen = observer(function JokeScreen() {
   const route = useRoute<NavigationProps<"JokeScreen">["route"]>()
   __DEV__ && console.tron.log!(route.params?.jokeId)
-  const translateX = useSharedValue(0)
-  const translateY = useSharedValue(0)
-  const rotate = useSharedValue(0)
-  // const { data } = useQuery((store) =>
-  //   store.queryJokes({ jokeLength: JokeLength.MEDIUM },
-  //     nodes(jokeModelPrimitives)
-  //   ),
-  // )
-  const animatedStyles = useAnimatedStyle(() => ({
-    transform: [
-      { translateX: translateX.value },
-      { translateY: -translateY.value },
-      // { rotateZ: `${rotate.value}deg` },
-    ],
-  }))
+  const [cards, setCards] = React.useState(jokes)
+
+  const ref = React.useRef<Animated.View>(null)
+  console.tron.log("ref: ", ref)
+
+  const { data } = useQuery((store) =>
+    store.queryJokes({ jokeLength: JokeLength.MEDIUM }, nodes(jokeModelPrimitives)),
+  )
 
   const handleBad = () => {
     translateX.value = withTiming(width * 1.2)
@@ -74,31 +67,45 @@ export const JokeScreen = observer(function JokeScreen() {
     rotate.value = withSpring(1)
   }
 
+  const translateX = useSharedValue(0)
+  const translateY = useSharedValue(0)
+  const rotate = useSharedValue(0)
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: translateX.value },
+      { translateY: -translateY.value },
+      { rotateZ: `${rotate.value}deg` },
+    ],
+  }))
+
   return (
-      <CenterView style={CONTAINER}>
-        <View style={HEADER}>
-          <Text text="Dad Jokes" style={CATEGORY_NAME} bold />
-          <Text h2 text="The Car Joke" bold />
-        </View>
-
-        <View style={{ position: "relative", flex: 1, width: "100%" }}>
-          {jokes.map(({ id, body }, index) => (
-            <Animated.View key={id} style={[StyleSheet.absoluteFillObject, animatedStyles]}>
-              <Card style={CARD} {...{ index, body }} />
-            </Animated.View>
-          ))}
-        </View>
-
-        <View style={JOKE_INFO}>
-          <Ratings likes={0} dislikes={0} />
-          {/* TODO: replace with actual id */}
-          <ShareIcons jokeId={route.params?.jokeId ?? ""} />
-        </View>
-
-        <Controls onBad={handleBad} />
-        <AdBanner />
-      </CenterView>
     <Screen style={ROOT} preset="fixed" testID="JokeScreen" unsafe>
+      <View style={HEADER}>
+        <Text text="Dad Jokes" style={CATEGORY_NAME} bold />
+        <Text h2 text="The Car Joke" bold />
+      </View>
+
+      <View style={{ position: "relative", flex: 1, width: "100%" }}>
+        {cards.map(({ id, body }, index) => (
+          <Card
+            key={id}
+            style={CARD}
+            animatedStyles={animatedStyles}
+            {...{ index, body }}
+            // ref={ref}
+          />
+        ))}
+      </View>
+
+      <View style={JOKE_INFO}>
+        <Ratings likes={0} dislikes={0} />
+        {/* TODO: replace with actual id */}
+        <ShareIcons jokeId={route.params?.jokeId ?? ""} />
+      </View>
+
+      <Controls onBad={handleBad} />
+      <AdBanner />
     </Screen>
   )
 })
@@ -106,15 +113,14 @@ export const JokeScreen = observer(function JokeScreen() {
 const ROOT: ViewStyle = {
   backgroundColor: color.background,
   flex: 1,
-}
-
-const CONTAINER: ViewStyle = {
   justifyContent: "space-between",
+  alignItems: "center",
 }
 
 const HEADER: ViewStyle = {
   alignItems: "center",
   justifyContent: "center",
+  paddingVertical: spacing[6],
 }
 
 const CARD: ViewStyle = {
@@ -148,21 +154,21 @@ export const Controls = (props: ButtonsProps) => {
 
   return (
     <View style={BUTTONS}>
-      <CircularButton style={DISLIKE} onPress={rate}>
+      <CircularButton style={DISLIKE} onPress={rate}activeOpacity={0.8}>
         <CryingEmoji />
       </CircularButton>
 
       <View style={SECONDARY_ACTION_BUTTONS}>
-        <CircularButton size="small" onPress={skip}>
+        <CircularButton size="small" onPress={skip} activeOpacity={0.8}>
           <BookmarkButton />
         </CircularButton>
 
-        <CircularButton size="small" onPress={bookmark}>
+        <CircularButton size="small" onPress={bookmark}activeOpacity={0.8}>
           <Skip />
         </CircularButton>
       </View>
 
-      <CircularButton onPress={rate}>
+      <CircularButton onPress={rate}activeOpacity={0.8}>
         <LaughingEmoji />
       </CircularButton>
     </View>
