682ca4ead0623c4f115af98cf40df06baa77fb0f
Kieran Osgood <kieranbosgood@gmail.com>
2021-07-23 18:52:32 +0100

    fix: issue with layout shift due to admob banner not having a minheight

--- app/components/ad-banner/ad-banner.tsx
+++ app/components/ad-banner/ad-banner.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { ViewStyle } from "react-native"
-import { observer } from "mobx-react-lite"
 import { BannerAd, BannerAdSize, TestIds } from "@react-native-firebase/admob"
-import { isProduction } from 'app/utils/current-environment'
+import { isProduction } from "app/utils/current-environment"
+import { observer } from "mobx-react-lite"
+import * as React from "react"
+import { View, ViewStyle } from "react-native"
 
 export interface AdBannerProps {
   /**
@@ -17,5 +17,18 @@ export interface AdBannerProps {
 export const AdBanner = observer(function AdBanner(props: AdBannerProps) {
   const unitId = isProduction ? "ca-app-pub-3681973098458031/3206308809" : TestIds.BANNER
 
-  return <BannerAd {...{ unitId }} size={BannerAdSize.SMART_BANNER} />
+  return (
+    <View style={{ minHeight: 50 }}>
+      <BannerAd
+        {...{ unitId }}
+        size={BannerAdSize.SMART_BANNER}
+        // prop types require these to be added
+        onAdClosed={() => {}}
+        onAdFailedToLoad={() => {}}
+        onAdLeftApplication={() => {}}
+        onAdLoaded={() => {}}
+        onAdOpened={() => {}}
+      />
+    </View>
+  )
 })
