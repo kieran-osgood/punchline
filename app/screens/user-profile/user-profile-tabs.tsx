ba39582bccfd070a1f77b704e4c1c107c7107ac5
Kieran Osgood <kieranbosgood@gmail.com>
2021-08-29 10:01:52 +0100

    feat(user-profile-tabs): remove icons from tabs

--- app/screens/user-profile/user-profile-tabs.tsx
+++ app/screens/user-profile/user-profile-tabs.tsx
@@ -3,7 +3,6 @@ import { observer } from "mobx-react-lite"
 import React from "react"
 import { ViewStyle } from "react-native"
 import { ThemeManager } from "react-native-ui-lib"
-import Icon from "react-native-vector-icons/AntDesign"
 import { color } from "theme"
 import { BookmarksScreen } from "../bookmarks/bookmarks-screen"
 import { HistoryScreen } from "../history/history-screen"
@@ -38,20 +37,24 @@ export const UserProfileTabs = observer(function UserProfileTabs() {
       <Tab.Screen
         name="Bookmarks"
         component={BookmarksScreen}
-        options={{
-          tabBarIcon: function TabIcon({ color }) {
-            return <Icon name="staro" {...{ color, size }} />
-          },
-        }}
+        options={
+          {
+            // tabBarIcon: function TabIcon({ color }) {
+            //   return <Icon name="staro" {...{ color, size }} />
+            // },
+          }
+        }
       />
       <Tab.Screen
         name="History"
         component={HistoryScreen}
-        options={{
-          tabBarIcon: function TabIcon({ color }) {
-            return <Icon name="book" {...{ color, size }} />
-          },
-        }}
+        options={
+          {
+            // tabBarIcon: function TabIcon({ color }) {
+            //   return <Icon name="book" {...{ color, size }} />
+            // },
+          }
+        }
       />
     </Tab.Navigator>
   )
