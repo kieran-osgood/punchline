248e34d08ba632e19b82bd5b6f8d9604052e487f
Kieran Osgood <kieranbosgood@gmail.com>
2021-09-20 11:47:49 +0100

    fix: react-native-restart types not being declared/found

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,5 +1,3 @@
-import { RootParamList as MyRootParamList } from "app/navigators"
-
 declare module "*.svg" {
   import { SvgProps } from "react-native-svg"
   const content: React.FC<SvgProps>
@@ -14,7 +12,14 @@ declare module "react-native-dotenv" {
 }
 
 declare global {
+  import { RootParamList as MyRootParamList } from "app/navigators"
   namespace ReactNavigation {
     interface RootParamList extends MyRootParamList {}
   }
+}
+declare module "react-native-restart" {
+  type RestartType = {
+    Restart(): void
+  }
+  export const RNRestart: RestartType
 }
