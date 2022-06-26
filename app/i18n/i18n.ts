99145edb064f99a4541a379a6b12fc65d78cb2af
Kieran Osgood <kieran.osgood@and.digital>
2022-06-26 14:07:57 +0100

    chore: remove recursive typescript type

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -12,11 +12,11 @@ i18n.locale = Localization.locale || "en"
  * Builds up valid keypaths for translations.
  * Update to your default locale of choice if not English.
  */
-type DefaultLocale = typeof en
-export type TxKeyPath = RecursiveKeyOf<DefaultLocale>
+// type DefaultLocale = typeof en
+// export type TxKeyPath = RecursiveKeyOf<DefaultLocale>
 
-type RecursiveKeyOf<TObj extends Record<string, any>> = {
-  [TKey in keyof TObj & string]: TObj[TKey] extends Record<string, any>
-    ? `${TKey}` | `${TKey}.${RecursiveKeyOf<TObj[TKey]>}`
-    : `${TKey}`
-}[keyof TObj & string]
+// type RecursiveKeyOf<TObj extends Record<string, any>> = {
+//   [TKey in keyof TObj & string]: TObj[TKey] extends Record<string, any>
+//     ? `${TKey}` | `${TKey}.${RecursiveKeyOf<TObj[TKey]>}`
+//     : `${TKey}`
+// }[keyof TObj & string]
--- app/i18n/translate.ts
+++ app/i18n/translate.ts
@@ -1,12 +1,12 @@
 import i18n from "i18n-js"
-import { TxKeyPath } from "./i18n"
+// import { TxKeyPath } from "./i18n"
 
 /**
  * Translates text.
  *
  * @param key The i18n key.
  */
-export function translate(key: TxKeyPath, options?: I18n.TranslateOptions) {
+export function translate(key: string, options?: I18n.TranslateOptions) {
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   return key ? i18n.t(key, options) : null
 }
